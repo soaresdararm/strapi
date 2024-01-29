0c007db27712fb2eba4fe8db0e6875bd2fca5d28
Dara Maria Rodrigues Soares <dara.soares@fitbank.com.br>
2024-01-29 16:41:31 -0300

    feat: configurações do strapi

--- strapi-landing-pages/config/database.ts
+++ strapi-landing-pages/config/database.ts
@@ -68,7 +68,7 @@ export default ({ env }) => {
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: { min: env.int('DATABASE_POOL_MIN', 0), max: env.int('DATABASE_POOL_MAX', 5) },
     },
     sqlite: {
       connection: {
